857ac5ef9f2e23881535b96edcf64dc75f4a6589
vibhor kumar <vibhork1105@gmail.com>
2025-06-10 13:44:17 +0530

    auth done (mostly)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 import { PrismaClient } from './generated/prisma';
 import authRoutes from './routes/authRoutes';
 
-dotenv.config();``
+dotenv.config();
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
     res.json({

a7cd73602c6cec89f73c1908cf49ef6b26ad0d73
vibhor kumar <vibhork1105@gmail.com>
2025-06-17 22:21:56 +0530

    recommendation engine started

---
+++ pre_training/base/base_model.py
@@ -0,0 +1,52 @@
+import polars as pl
+import torch 
+from abc import ABC, abstractmethod
+
+class RecommenderModel(ABC):
+
+    @abstractmethod
+    def build(self):
+        """
+        Build the model architecture.
+        This method should be implemented by subclasses to define the model structure.
+        """
+        pass
+
+    @abstractmethod
+    def train(self,train_df:pl.DataFrame,val_loader:pl.DataFrame | None = None):
+        """
+        Train the model.
+        This method should be implemented by subclasses to define the training process.
+        """
+        pass
+
+    @abstractmethod
+    def test(self,test_df:pl.DataFrame):
+        """
+        Test the model.
+        This method should be implemented by subclasses to define the testing process.
+        """
+        pass
+
+    @abstractmethod
+    def predict(self, user_ids: torch.Tensor, item_ids: torch.Tensor | None) -> torch.Tensor:
+        """
+        Predict scores for given user and item IDs.
+        This method should be implemented by subclasses to define the prediction logic.
+        """
+        pass
+
+    @abstractmethod
+    def save(self,save_path: str):
+        """
+        Save the model.
+        This method should be implemented by subclasses to define how the model is saved.
+        """
+        pass
+
+    def load(self, load_path: str):
+        """
+        Load the model.
+        This method should be implemented by subclasses to define how the model is loaded.
+        """
+        pass

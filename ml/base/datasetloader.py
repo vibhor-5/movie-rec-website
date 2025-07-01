f85f9cb8f60ea6368fc862defabaf94c1bb50fd6
vibhor kumar <vibhork1105@gmail.com>
2025-07-01 13:56:49 +0530

    idek what im doing man

--- ml/base/datasetloader.py
+++ ml/base/datasetloader.py
@@ -75,8 +75,8 @@ class DatasetLoader:
             data = data.with_columns(
                 pl.when(pl.col('rating') >= min_rating).then(1).otherwise(0).alias('rating')
             )
-        unique_users = data["user_id"].to_list()
-        unique_movies = data["movie_id"].to_list()
+        unique_users = data.get_column("user_id").to_list()
+        unique_movies = data.get_column("movie_id").to_list()
         uid_map= {uid: i for i, uid in enumerate(unique_users)}
         mid_map= {mid: i for i, mid in enumerate(unique_movies)}
 
